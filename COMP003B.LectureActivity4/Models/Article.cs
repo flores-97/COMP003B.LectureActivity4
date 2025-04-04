253aa35779fa53df8c0f15eeee9b7e925178dc80
flores-97 <floresvictor15@gmail.com>
2025-04-04 11:11:08 -0700

    created article model

---
+++ COMP003B.LectureActivity4/Models/Article.cs
@@ -0,0 +1,16 @@
+﻿namespace COMP003B.LectureActivity4.Models
+{
+    using System.ComponentModel.DataAnnotations;
+    public class Article
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Title { get; set; }
+
+        [Required]
+        [StringLength (300)]
+        public string Summary {  get; set; }
+    }
+}
