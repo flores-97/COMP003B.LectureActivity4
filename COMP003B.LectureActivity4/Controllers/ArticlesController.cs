efeb1f7be11f48fe5dbfe05c46ae2948036b3654
flores-97 <floresvictor15@gmail.com>
2025-04-07 19:49:44 -0700

    corrections made after class

--- COMP003B.LectureActivity4/Controllers/ArticlesController.cs
+++ COMP003B.LectureActivity4/Controllers/ArticlesController.cs
@@ -7,10 +7,10 @@
         public IActionResult Index()
         {
             var articles = new List<Article>
-        {
-            new Article { Id = 1, Title = "Intro to Razor", Summary = "Learn the basics of Razor syntax." },
-            new Article { Id = 2, Title = "Layouts and Partials", Summary = "Understand Razor layouts and reusable views." }
-        };
+            {
+                new Article { Id = 1, Title = "Intro to Razor", Summary = "Learn the basics of Razor syntax." },
+                new Article { Id = 2, Title = "Layouts and Partials", Summary = "Understand Razor layouts and reusable views." }
+            };
 
             return View(articles);
         }
